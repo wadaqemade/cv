546767b9c6677c5587548981c340aed18bbb1922
wadaqemade_1 <www.hambash@gmail.com>
2019-08-20 22:17:35 +0300

    edit some files

--- cv/urls.py
+++ cv/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView # new
+from django.views.generic.base import TemplateView 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')), # new
+    path('', include('accounts.urls')), 
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='base.html'), name='base'), # new
 
 ]
